f8910863e7d56a362c8e5ac1efca52aa9c18ddb8
Takafumi Okazaki <tokazaki@c1r27s3.42tokyo.jp>
2023-07-09 16:42:57 +0900

    return

--- numeron.c
+++ numeron.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-<<<<<<< HEAD
-int hasDuplicate(char guess[]) {
-	// for (int i = 0; guess[i] != '\0' ; i++)
-	// {
-	// 	if (guess[i] < '0' || '9' < guess[i] || 10 < i)
-	// 		return 1;
-	// }
-	for (int i = 0; i < 4; i++) {
-=======
+// <<<<<<< HEAD
+// int hasDuplicate(char guess[]) {
+// 	// for (int i = 0; guess[i] != '\0' ; i++)
+// 	// {
+// 	// 	if (guess[i] < '0' || '9' < guess[i] || 10 < i)
+// 	// 		return 1;
+// 	// }
+// 	for (int i = 0; i < 4; i++) {
+// =======
 int hasDuplicate(int guess[]) {
     for (int i = 0; i < 4; i++) {
->>>>>>> 53f40b6... OK
+// >>>>>>> 53f40b6... OK
         for (int j = i + 1; j < 4; j++) {
             if (guess[i] == guess[j]) {
                 return 1; // 重複が見つかった場合、エラーとして1を返す
@@ -24,31 +24,30 @@ int hasDuplicate(int guess[]) {
 }
 
 int main() {
-<<<<<<< HEAD
-    char target[4];  // 目標の4桁の数字
-    char guess[4];   // プレイヤーの予想の4桁の数字
-=======
+// <<<<<<< HEAD
+//     char target[4];  // 目標の4桁の数字
+//     char guess[4];   // プレイヤーの予想の4桁の数字
+// =======
     int target[4];  // 目標の4桁の数字
     int guess[4];   // プレイヤーの予想の4桁の数字
->>>>>>> 53f40b6... OK
+// >>>>>>> 53f40b6... OK
     int attempts = 0;  // プレイヤーの試行回数
-
     srand(time(0));
 
-<<<<<<< HEAD
-    target[0] = rand() % 10 + 48;
-
-    do {
-        target[1] = rand() % 10 + 48;
-    } while (target[1] == target[0]);
-
-    do {
-        target[2] = rand() % 10 + 48;
-    } while (target[2] == target[0] || target[2] == target[1]);
-
-    do {
-        target[3] = rand() % 10 + 48;
-=======
+//<<<<<<< HEAD
+//    target[0] = rand() % 10 + 48;
+//
+//    do {
+//        target[1] = rand() % 10 + 48;
+//    } while (target[1] == target[0]);
+//
+//    do {
+//        target[2] = rand() % 10 + 48;
+//    } while (target[2] == target[0] || target[2] == target[1]);
+//
+//    do {
+//        target[3] = rand() % 10 + 48;
+//=======
     target[0] = rand() % 10;
 
     do {
@@ -61,9 +60,9 @@ int main() {
 
     do {
         target[3] = rand() % 10;
->>>>>>> 53f40b6... OK
+// >>>>>>> 53f40b6... OK
     } while (target[3] == target[0]  || target[3] == target[1]|| target[3] == target[2]);
-
+printf("%d%d%d%d\n",target[0],target[1],target[2],target[3]);
     printf("数当てゲームを開始します！\n");
     printf("0から9までの数字を使って4桁の数を当ててください。\n");
 
@@ -72,13 +71,13 @@ int main() {
         int breaks = 0;  // ブレークの数
 
         printf("\n4桁の数字を入力してください（重複は不可）: ");
-<<<<<<< HEAD
-        if (scanf("%1c%1c%1c%1c", &guess[0], &guess[1], &guess[2], &guess[3]) != 4 || hasDuplicate(guess)) {
-            printf("入力が無効です。正しい4桁の数字を正しく入力してください。\n");
-=======
+// <<<<<<< HEAD
+//         if (scanf("%1c%1c%1c%1c", &guess[0], &guess[1], &guess[2], &guess[3]) != 4 || hasDuplicate(guess)) {
+//             printf("入力が無効です。正しい4桁の数字を正しく入力してください。\n");
+// =======
         if (scanf("%1d%1d%1d%1d", &guess[0], &guess[1], &guess[2], &guess[3]) != 4 || hasDuplicate(guess)) {
             printf("入力が無効です。重複があるか、4桁の数字を正しく入力してください。\n");
->>>>>>> 53f40b6... OK
+// >>>>>>> 53f40b6... OK
             fflush(stdin); // 入力バッファをクリア
             continue; // ループを継続して入力をやり直す
         }
